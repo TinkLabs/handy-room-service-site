6e8f5aaac4e4a66e611f6ce8b8799cd8f189e619
Allen Wong <alnaym30@gmail.com>
2018-10-30 15:17:49 +0900

    Fix delivery time box overlap error

--- src/components/DeliveryTimeBox/index.js
+++ src/components/DeliveryTimeBox/index.js
@@ -9,25 +9,28 @@ const propTypes = {
 	deliveryTimeSlotEnd: PropTypes.string,
 	deliveryTimeSlotStart: PropTypes.string,
 	locale: PropTypes.string,
+	className: PropTypes.string,
 };
 
 const defaultProps = {
 	deliveryTimeSlotEnd: '',
 	deliveryTimeSlotStart: '',
 	locale: 'en_US',
+	className: '',
 };
 
 const DeliveryTimeBox = ({
 	deliveryTimeSlotEnd,
 	deliveryTimeSlotStart,
 	locale,
+	className,
 }) => {
 	if (!deliveryTimeSlotEnd || !deliveryTimeSlotStart) return null;
 	const shortLocale = locale.substring(0, 2);
 	const from = converthhmmAToHHmm(deliveryTimeSlotStart, shortLocale);
 	const to = converthhmmAToHHmm(deliveryTimeSlotEnd, shortLocale);
 	return (
-		<div className={classnames('delivery-time-slot')}>
+		<div className={classnames('delivery-time-slot', className)}>
 			<span className={classnames(classnames(styles.clock), 'icon icon-handy-icon-clock')} />
 			<span>{`${from} - ${to}`}</span>
 		</div>
