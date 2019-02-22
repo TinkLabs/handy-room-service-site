c25877b3e4eb090210fbfd917b49748e2ddbc16d
Allen Wong <alnaym30@gmail.com>
2019-02-22 22:21:45 +0800

    add locale to React scope

--- src/pages/OrderHistoryPage/DatetimeHr/index.js
+++ src/pages/OrderHistoryPage/DatetimeHr/index.js
@@ -3,18 +3,18 @@ import moment from 'moment-timezone';
 import { connect } from 'react-redux';
 import styles from './style.scss';
 
-moment.locale('en', {
-	calendar: {
-		lastDay: '[Yesterday,] MMMM D YYYY',
-		lastWeek: '[Last] dddd[,] MMMM D YYYY',
-		sameDay: '[Today]',
-		sameElse: 'MMMM D YYYY',
-	},
-});
 
 class DatetimeHr extends React.Component {
 	constructor(props) {
 		super(props);
+		moment.locale('en', {
+			calendar: {
+				lastDay: '[Yesterday,] MMMM D YYYY',
+				lastWeek: '[Last] dddd[,] MMMM D YYYY',
+				sameDay: '[Today]',
+				sameElse: 'MMMM D YYYY',
+			},
+		});
 		moment.tz.setDefault(props.timezone);
 	}
 	render() {
